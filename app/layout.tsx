a4b9188599896275783134e1795f0dbe4d49b873
Freddy Amin Zapata <83481744+goku673@users.noreply.github.com>
2025-06-10 12:51:25 -0400

    changes v3

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'sis-eco',
   description: 'Created with v0',
-  generator: 'v0.dev',
+  generator: 'fazm',
 }
 
 export default function RootLayout({
